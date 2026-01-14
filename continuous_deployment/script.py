6b70e5cb645387a70b415e03adcb66fab9aeb74e
Alexander Leonhardt <equinox.salexander@gmail.com>
2026-01-14 10:59:32 +0100

    Fixed typo

--- continuous_deployment/script.py
+++ continuous_deployment/script.py
@@ -64,7 +64,7 @@ class BuildServer(http.server.BaseHTTPRequestHandler):
                 print(f"Registered push to {payload['ref']}, but the branch was not among the tracked branches",file=sys.stderr)
         elif self.headers.get('X-GitHub-Event') == 'pull_request':
             if payload["action"] == "opened" or payload["action"] == "synchronize":
-                if payload["action"] == "openend":
+                if payload["action"] == "opened":
                     path = f"/var/www/localhost/htdocs/logs/pr/{payload['number']}/{payload['pull_request']['head']['sha']}"
                 else:
                     path = f"/var/www/localhost/htdocs/logs/pr/{payload['number']}/{payload['after']}"
