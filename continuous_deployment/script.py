f71ffc6a3b604401dd52db805b7daca66baca7a5
Alexander Leonhardt <equinox.salexander@gmail.com>
2026-01-14 10:39:30 +0100

    Changed error handling in favor of specific HTML return codes

--- continuous_deployment/script.py
+++ continuous_deployment/script.py
@@ -21,11 +21,12 @@ def verify_signature(payload_body, secret_token, signature_header):
         signature_header: header received from GitHub (x-hub-signature-256)
     """
     if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+        return False
     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
     if not hmac.compare_digest(expected_signature, signature_header):
-        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+        return False
+    return True
 
 class BuildServer(http.server.BaseHTTPRequestHandler):
     secret_token = ""
@@ -40,7 +41,10 @@ class BuildServer(http.server.BaseHTTPRequestHandler):
            return
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
-        verify_signature(post_body, BuildServer.secret_token, self.headers.get("X-Hub-Signature-256"))
+        if not verify_signature(post_body, BuildServer.secret_token, self.headers.get("X-Hub-Signature-256")):
+            self.send_error(403)
+            self.end_headers()
+            return
         payload = json.loads(post_body.decode("utf-8"))
 
 
